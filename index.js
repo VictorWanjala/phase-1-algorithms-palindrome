582b70cbc0b98b9fb9e05549666c4b0c897329fe
Victor <wanjala94vic@gmail.com>
2023-07-28 12:47:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedText = "";
+  for(let i=word.length-1; i>=0; i--){
+    reversedText += word[i];
+  }
+  return word === reversedText
 }
 
 /* 
-  Add your pseudocode here
+  create a variable to store reversed word
+  loop through characters
+  append the last character to variable reversedText
+  if equal, return true, otherwise return false
 */
 
 /*
   Add written explanation of your solution here
+  the function checks whether the input is a palindrome or not
+  Checks are done to confirm if the word would be the same id reversed
+  This means that the words would read the same forward and backwards hence a
+  palindrome.
 */
 
 // You can run `node index.js` to view these console logs
